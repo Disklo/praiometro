f33c69f61cbe70c99467ce08ba069e7c1a255d2f
Disklo <rafaellcarvalho5@gmail.com>
2025-07-15 17:59:25 -0300

    import error fix

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,12 @@
 import StackNavigator from './src/StackNavigator';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { useEffect } from 'react';
+import Constants from 'expo-constants';
 
 export default function App() {
     useEffect(() => {
         GoogleSignin.configure({
-            webClientId: process.env.GOOGLE_CLIENT_ID,
+            webClientId: Constants.expoConfig.extra.GOOGLE_CLIENT_ID,
             offlineAccess: true,
         });
     }, []);
